8741261d8eafe116a04fab6c61794963529ea861
Alejandra Colcha <daniela.colcha@epn.edu.ec>
2025-01-21 21:10:24 -0500

    configuracion de cabeceras seguras

--- next.config.mjs
+++ next.config.mjs
@@ -3,42 +3,27 @@ const nextConfig = {
   env: {
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   },
-
-  // async redirects () {
-  //   return [
-  //     {
-  //       source: '/',
-  //       destination: '/maintenance',
-  //       permanent: false,
-  //     },
-  //     {
-  //       source: '/login',
-  //       destination: '/maintenance',
-  //       permanent: false,
-  //     },
-  //     {
-  //       source: '/panel',
-  //       destination: '/maintenance',
-  //       permanent: false,
-  //     },
-  //     {
-  //       source: '/panel/reporte',
-  //       destination: '/maintenance',
-  //       permanent: false,
-  //     },
-  //     {
-  //       source: '/localidades',
-  //       destination: '/maintenance',
-  //       permanent: false,
-  //     },
-  //     {
-  //       source: '/localidades/general',
-  //       destination: '/maintenance',
-  //       permanent: false,
-  //     },
-  //   ]
-  // }
-
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self';"
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: "X-Powered-By",
+            value: ""
+          }
+        ],
+      },
+    ];
+  },
 
 };
 
